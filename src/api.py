9cbd2dd712a629cdb7775b499a1f26e84d4a46b2
OleksandrKutsevHYS <oleksandr.kutsev@hys-enterprise.com>
2025-11-06 16:33:40 +0300

    Delete src/api.py

--- src/api.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-
-@app.route("/api/", methods=["GET"])
-def get_users():
-    return jsonify({"response":"get this app for FREE from TotallyNotScamWebsite.com, much better than here"})
